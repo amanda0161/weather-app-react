661bf079e1dccdca47a81f33239137c7bfa8030e
AC <amanda.charlesworth1@gmail.com>
2022-03-07 20:07:31 +0100

    Fixed temperature display

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,33 +5,31 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div>
-      <div className="current-city">
-        <h1>{props.data.city}</h1>
-        <p>
+    <div className="current-city">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
           <FormattedDate date={props.data.date} />
-        </p>
-      </div>
-
-      <div className="row">
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix float-right">
-            <ul>
-              <li>{props.data.description}</li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
         </div>
         <div className="col-6">
-          <div className="weather-temperature">
-            <span>
-              <WeatherTemperature celcius={props.data.temperature} />
-            </span>
-          </div>
-          <span className="weather-icon">
-            <WeatherIcon code={props.data.icon} alt={props.data.description} />
-          </span>
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
       </div>
     </div>
