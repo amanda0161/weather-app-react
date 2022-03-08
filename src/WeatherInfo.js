fc65423ac9b3f76dc8860ba7e7980909b89e6a4f
AC <amanda.charlesworth1@gmail.com>
2022-03-08 18:05:47 +0100

    Using flex

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,19 +13,13 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
-            </div>
 
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
-            </div>
-          </div>
+      <div className="flex">
+        <div className="flex__col">
+          <WeatherIcon code={props.data.icon} size={52} />{" "}
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
-        <div className="col-6">
+        <div className="flex__col">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
