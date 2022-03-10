73533bd333afeec5a9ef5f215d7ece9e048d00a2
AC <amanda.charlesworth1@gmail.com>
2022-03-10 21:59:26 +0100

    Added Weather Forecast and Forecast Day components

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -9,6 +10,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
@@ -56,7 +58,8 @@ export default function Weather(props) {
             </div>
           </form>
         </div>
-        <WeatherInfo data={weatherData} />
+        <WeatherInfo data={weatherData} size={64} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
